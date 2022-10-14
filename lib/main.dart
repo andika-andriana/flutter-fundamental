ca98d9ef5e3c06554f7f8ecaa9aaa7083a9d8db3
Andika Andriana <andriana.mia1@gmail.com>
2022-10-14 08:48:38 +0700

    + Stack Layout Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +7,119 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text(
+          "Stack Layout Example",
+          style: TextStyle(fontFamily: "Poppins"),
+        )),
+        body: Stack(
+          // BACKGROUND
+          children: [
+            Column(
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black38,
+                            )),
+                      ],
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black38,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            )),
+                      ],
+                    )),
+              ],
+            ),
+
+            // TEXT-LISTVIEW
+            ListView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Apakah Lorem Ipsum itu?",
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 24,
+                            color: Colors.black87),
+                      ),
+                      Text(
+                        "Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum",
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 16,
+                            color: Colors.black54),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Dari mana asalnya?",
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 24,
+                            color: Colors.black87),
+                      ),
+                      Text(
+                        "Tidak seperti anggapan banyak orang, Lorem Ipsum bukanlah teks-teks yang diacak. Ia berakar dari sebuah naskah sastra latin klasik dari era 45 sebelum masehi, hingga bisa dipastikan usianya telah mencapai lebih dari 2000 tahun. Richard McClintock, seorang professor Bahasa Latin dari Hampden-Sidney College di Virginia, mencoba mencari makna salah satu kata latin yang dianggap paling tidak jelas, yakni consectetur, yang diambil dari salah satu bagian Lorem Ipsum. Setelah ia mencari maknanya di di literatur klasik, ia mendapatkan sebuah sumber yang tidak bisa diragukan. Lorem Ipsum berasal dari bagian 1.10.32 dan 1.10.33 dari naskah 'de Finibus Bonorum et Malorum' (Sisi Ekstrim dari Kebaikan dan Kejahatan) karya Cicero, yang ditulis pada tahun 45 sebelum masehi. BUku ini adalah risalah dari teori etika yang sangat terkenal pada masa Renaissance. Baris pertama dari Lorem Ipsum, 'Lorem ipsum dolor sit amet..', berasal dari sebuah baris di bagian 1.10.32.\n\nBagian standar dari teks Lorem Ipsum yang digunakan sejak tahun 1500an kini di reproduksi kembali di bawah ini untuk mereka yang tertarik. Bagian 1.10.32 dan 1.10.33 dari 'de Finibus Bonorum et Malorum' karya Cicero juga di reproduksi persis seperti bentuk aslinya, diikuti oleh versi bahasa Inggris yang berasal dari terjemahan tahun 1914 oleh H. Rackham.",
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 16,
+                            color: Colors.black54),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: const Alignment(0.9, 0.9),
+              child: ElevatedButton(
+                child: const Text("My Button"),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleLayoutExample extends StatelessWidget {
+  const FlexibleLayoutExample({super.key});
 
   @override
   Widget build(BuildContext context) {
