8a04b53c4aed22379637704e4cb29ceb4df4d175
Andika Andriana <andriana.mia1@gmail.com>
2022-10-16 12:41:21 +0700

    + Add Image to AppBar Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.adb,
+          size: 18,
+          color: Colors.white,
+        ),
+        title: const Text(
+          "Pattern AppBar",
+          style: TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+              size: 18,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+              size: 18,
+            ),
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.white, Colors.grey],
+              begin: FractionalOffset.topCenter,
+              end: FractionalOffset.bottomCenter,
+            ),
+            image: DecorationImage(
+              image: AssetImage("images/app_bar_bg.png"),
+              fit: BoxFit.none,
+              repeat: ImageRepeat.repeat,
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class MaterialNavigatorExample extends StatelessWidget {
+  const MaterialNavigatorExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: LoginPage(),
     );
