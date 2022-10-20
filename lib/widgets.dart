081d2d09ac461a4017c24f5494333c3cc18ff85f
Andika Andriana <andriana.mia1@gmail.com>
2022-10-20 07:24:40 +0700

    + Custom Card Example

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -2,6 +2,84 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
 
+class InkWellExample extends StatelessWidget {
+  const InkWellExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "InkWell Example",
+            style: TextStyle(fontFamily: "Poppins"),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  backgroundColor: Colors.blueAccent.shade700,
+                ),
+                onPressed: () {},
+                child: Container(
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  child: const Text(
+                    "Test",
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(6),
+                elevation: 1,
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    gradient: const LinearGradient(
+                      colors: [Colors.blueAccent, Colors.amber],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.brown.shade100,
+                      borderRadius: BorderRadius.circular(6),
+                      onTap: () {},
+                      child: const Center(
+                        child: Text(
+                          "Test Ink Well",
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class MediaQueryExample extends StatelessWidget {
   const MediaQueryExample({super.key});
 
