b2f6faad42c493f22e013823bb686fc6a19fb08d
Andika Andriana <andriana.mia1@gmail.com>
2023-02-26 00:30:31 +0700

    + Login Page Example

--- lib/main.dart
+++ lib/main.dart
@@ -10,173 +10,219 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CustomCardExample(),
+      debugShowCheckedModeBanner: false,
+      home: LoginExample(),
     );
   }
 }
 
-class CustomCardExample extends StatelessWidget {
-  const CustomCardExample({super.key});
+class LoginExample extends StatelessWidget {
+  const LoginExample({super.key});
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getLargeDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Custom Card Example",
-          style: TextStyle(
-            fontFamily: "Poppins",
-          ),
-        ),
-      ),
+      backgroundColor: const Color.fromARGB(255, 21, 21, 21),
       body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.blue,
-                  Colors.white,
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+        children: <Widget>[
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [Colors.pink, Colors.blue],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  )),
+            ),
+          ),
+          Positioned(
+            left: -getLargeDiameter(context) / 4,
+            top: -getLargeDiameter(context) / 4,
+            child: Container(
+              width: getLargeDiameter(context),
+              height: getLargeDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [Colors.pink, Colors.blue],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  )),
+              child: const Center(
+                child: Text(
+                  "IS MY LIFE",
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 30,
+                    fontFamily: "Poppins",
+                    decoration: TextDecoration.underline,
+                    decorationStyle: TextDecorationStyle.double,
+                    decorationThickness: 0.5,
+                    decorationColor: Colors.amber,
+                  ),
+                ),
               ),
             ),
           ),
-          Center(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.6,
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          spreadRadius: 5,
-                          blurRadius: 5,
-                          offset: Offset(0, 5), // changes position of shadow
+          Positioned(
+            right: -getSmallDiameter(context) / 4,
+            bottom: -getSmallDiameter(context) / 4,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(40, 255, 2, 192),
+              ),
+            ),
+          ),
+          Align(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(167, 159, 153, 153),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(
+                    children: const <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Colors.white60,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white30),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.pink)),
+                          labelText: "Email ",
+                          labelStyle: TextStyle(color: Colors.white70),
                         ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      image: DecorationImage(
-                        image: AssetImage("images/app_bar_bg.png"),
-                        fit: BoxFit.fill,
-                        opacity: 0.2,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.password,
+                            color: Colors.white60,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white30),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.pink)),
+                          labelText: "Password ",
+                          labelStyle: TextStyle(color: Colors.white70),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 20, 20, 20),
+                    child: const Text(
+                      "FORGOT PASSWORD? ",
+                      style: TextStyle(
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/default_images.jpeg"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(6),
-                        topRight: Radius.circular(6),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      left: 16,
-                      right: 16,
-                      top: (MediaQuery.of(context).size.height * 0.3) + 50,
-                      bottom: 16,
-                    ),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Dummy Photo from\nSome Place",
-                            maxLines: 2,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: "Poppins",
-                              fontWeight: FontWeight.normal,
-                              color: Colors.lightBlue,
-                              fontSize: 24,
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 40,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: const LinearGradient(
+                              colors: [Colors.pink, Colors.blue],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
                             ),
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  "Posted on ",
-                                  maxLines: 2,
-                                  textAlign: TextAlign.center,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(20),
+                              splashColor: Colors.blue,
+                              onTap: () {},
+                              child: const Center(
+                                child: Text(
+                                  "SIGN IN",
                                   style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.grey,
-                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
                                   ),
                                 ),
-                                Text(
-                                  "Oct 2022",
-                                  maxLines: 2,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.lightBlue,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            child: Row(
-                              children: const [
-                                Spacer(flex: 10),
-                                Icon(
-                                  Icons.thumb_up,
-                                  color: Colors.grey,
-                                  size: 14,
-                                ),
-                                Spacer(flex: 1),
-                                Text(
-                                  "100",
-                                  style: TextStyle(
-                                      fontFamily: "Poppins",
-                                      fontSize: 14,
-                                      color: Colors.grey),
-                                ),
-                                Spacer(flex: 1),
-                                Icon(
-                                  Icons.thumb_up,
-                                  color: Colors.grey,
-                                  size: 14,
-                                ),
-                                Spacer(flex: 1),
-                                Text(
-                                  "100",
-                                  style: TextStyle(
-                                      fontFamily: "Poppins",
-                                      fontSize: 14,
-                                      color: Colors.grey),
-                                ),
-                                Spacer(flex: 10),
-                              ],
-                            ),
-                          )
-                        ],
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {},
+                        mini: true,
+                        elevation: 5,
+                        child: const Image(
+                          image: AssetImage("images/facebook.png"),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {},
+                        mini: true,
+                        elevation: 5,
+                        child: const Image(
+                          image: AssetImage("images/twitter.png"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "DON'T HAVE ACCOUNT? ",
+                      style: TextStyle(
+                        color: Colors.white38,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                  ),
-                ],
-              ),
+                    Text(
+                      "SIGN UP",
+                      style: TextStyle(
+                        color: Colors.pink,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-          ),
+          )
         ],
       ),
     );
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -2,6 +2,174 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
 
+class CustomCardExample extends StatelessWidget {
+  const CustomCardExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Custom Card Example",
+          style: TextStyle(
+            fontFamily: "Poppins",
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue,
+                  Colors.white,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.6,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                          offset: Offset(0, 5), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      image: DecorationImage(
+                        image: AssetImage("images/app_bar_bg.png"),
+                        fit: BoxFit.fill,
+                        opacity: 0.2,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/default_images.jpeg"),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(6),
+                        topRight: Radius.circular(6),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      top: (MediaQuery.of(context).size.height * 0.3) + 50,
+                      bottom: 16,
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Dummy Photo from\nSome Place",
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.normal,
+                              color: Colors.lightBlue,
+                              fontSize: 24,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "Posted on ",
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  "Oct 2022",
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.lightBlue,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 8),
+                            child: Row(
+                              children: const [
+                                Spacer(flex: 10),
+                                Icon(
+                                  Icons.thumb_up,
+                                  color: Colors.grey,
+                                  size: 14,
+                                ),
+                                Spacer(flex: 1),
+                                Text(
+                                  "100",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      color: Colors.grey),
+                                ),
+                                Spacer(flex: 1),
+                                Icon(
+                                  Icons.thumb_up,
+                                  color: Colors.grey,
+                                  size: 14,
+                                ),
+                                Spacer(flex: 1),
+                                Text(
+                                  "100",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      color: Colors.grey),
+                                ),
+                                Spacer(flex: 10),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class InkWellExample extends StatelessWidget {
   const InkWellExample({super.key});
 
@@ -39,7 +207,7 @@ class InkWellExample extends StatelessWidget {
               ),
               Material(
                 borderRadius: BorderRadius.circular(6),
-                elevation: 1,
+                elevation: 5,
                 child: Container(
                   width: 150,
                   height: 50,
