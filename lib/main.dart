8b57fb9430d78e1e224f4ff19b1401fe544b2e9f
Andika Andriana <andriana.mia1@gmail.com>
2023-02-26 08:15:10 +0700

    + Hero Animation aka Share Element

--- lib/main.dart
+++ lib/main.dart
@@ -10,220 +10,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginExample(),
+      home: HeroAnimationExample(),
     );
   }
 }
 
-class LoginExample extends StatelessWidget {
-  const LoginExample({super.key});
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-  double getLargeDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
+class HeroAnimationExample extends StatelessWidget {
+  const HeroAnimationExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 21, 21, 21),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
-            child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    colors: [Colors.pink, Colors.blue],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  )),
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: const Text(
+          "Hero Animation Example",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          Positioned(
-            left: -getLargeDiameter(context) / 4,
-            top: -getLargeDiameter(context) / 4,
-            child: Container(
-              width: getLargeDiameter(context),
-              height: getLargeDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    colors: [Colors.pink, Colors.blue],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  )),
-              child: const Center(
-                child: Text(
-                  "IS MY LIFE",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 30,
-                    fontFamily: "Poppins",
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.double,
-                    decorationThickness: 0.5,
-                    decorationColor: Colors.amber,
-                  ),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const HeroAnimationDetailExample();
+          }));
+        },
+        child: Hero(
+          tag: "avatar",
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100 / 2),
+            child: const SizedBox(
+              width: 100,
+              height: 100,
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  "https://media.licdn.com/dms/image/C5603AQE9N7B89s3HBQ/profile-displayphoto-shrink_400_400/0/1649749505592?e=1683158400&v=beta&t=ruw4t9nkui92bxL0_K8F7Jcmr7HBjONSE-Sr8mzwMJQ",
                 ),
               ),
             ),
           ),
-          Positioned(
-            right: -getSmallDiameter(context) / 4,
-            bottom: -getSmallDiameter(context) / 4,
-            child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color.fromARGB(40, 255, 2, 192),
+        ),
+      ),
+    );
+  }
+}
+
+class HeroAnimationDetailExample extends StatelessWidget {
+  const HeroAnimationDetailExample({super.key});
+  double getWidth(BuildContext context) => MediaQuery.of(context).size.width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: const Text(
+          "Hero Animation Detail Example",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      // ignore: sized_box_for_whitespace
+      body: Container(
+        width: getWidth(context),
+        height: getWidth(context),
+        child: const Hero(
+          tag: "avatar",
+          child: SizedBox(
+            child: Image(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                "https://media.licdn.com/dms/image/C5603AQE9N7B89s3HBQ/profile-displayphoto-shrink_400_400/0/1649749505592?e=1683158400&v=beta&t=ruw4t9nkui92bxL0_K8F7Jcmr7HBjONSE-Sr8mzwMJQ",
               ),
             ),
           ),
-          Align(
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(167, 159, 153, 153),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  child: Column(
-                    children: const <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.email,
-                            color: Colors.white60,
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white30),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.pink)),
-                          labelText: "Email ",
-                          labelStyle: TextStyle(color: Colors.white70),
-                        ),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.password,
-                            color: Colors.white60,
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white30),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.pink)),
-                          labelText: "Password ",
-                          labelStyle: TextStyle(color: Colors.white70),
-                        ),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(0, 20, 20, 20),
-                    child: const Text(
-                      "FORGOT PASSWORD? ",
-                      style: TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: const LinearGradient(
-                              colors: [Colors.pink, Colors.blue],
-                              begin: Alignment.centerLeft,
-                              end: Alignment.centerRight,
-                            ),
-                          ),
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.transparent,
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              splashColor: Colors.blue,
-                              onTap: () {},
-                              child: const Center(
-                                child: Text(
-                                  "SIGN IN",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 5,
-                        child: const Image(
-                          image: AssetImage("images/facebook.png"),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 5,
-                        child: const Image(
-                          image: AssetImage("images/twitter.png"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Text(
-                      "DON'T HAVE ACCOUNT? ",
-                      style: TextStyle(
-                        color: Colors.white38,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
