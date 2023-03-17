6ed5d2dbf09e27eb5970f2e3626022a1b75029ed
Andika Andriana <andriana.mia1@gmail.com>
2023-03-17 23:41:17 +0700

    + Generate QR Code Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,91 +11,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: TabBarExample(),
+      home: GenerateQrCodeExample(),
     );
   }
 }
 
-class TabBarExample extends StatelessWidget {
-  const TabBarExample({super.key});
+class GenerateQrCodeExample extends StatelessWidget {
+  const GenerateQrCodeExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(100),
-              child: Container(
-                color: Colors.black12,
-                child: const TabBar(
-                  labelColor: Colors.white,
-                  indicator: BoxDecoration(
-                      color: Colors.cyan,
-                      border: Border(
-                          bottom: BorderSide(
-                        color: Colors.black,
-                        width: 2,
-                      ))),
-                  indicatorColor: Colors.amberAccent,
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.home),
-                      text: 'Home',
-                    ),
-                    Tab(
-                      icon: Icon(Icons.book_online),
-                      text: 'Book',
-                    ),
-                    Tab(
-                      icon: Icon(Icons.shop),
-                      text: 'Shop',
-                    ),
-                    Tab(
-                      icon: Icon(Icons.person),
-                      text: 'Profile',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            title: Text(
-              'Top tab bar sample'.toUpperCase(),
-              style: const TextStyle(
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              Icon(
-                Icons.home,
-                color: Colors.black12,
-                size: 100,
-              ),
-              Icon(
-                Icons.book_online,
-                color: Colors.black12,
-                size: 100,
-              ),
-              Icon(
-                Icons.shop,
-                color: Colors.black12,
-                size: 100,
-              ),
-              Icon(
-                Icons.person,
-                color: Colors.black12,
-                size: 100,
-              ),
-            ],
-          ),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Generate QR Code Example"),
       ),
-    );
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Scan Me".toUpperCase(),
+              style: const TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          QrImage(
+            version: 6,
+            size: 300,
+            data: 'https://andika-andriana.github.io',
+          ),
+        ],
+      )),
+    ));
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -2,6 +2,90 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
 
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(100),
+              child: Container(
+                color: Colors.black12,
+                child: const TabBar(
+                  labelColor: Colors.white,
+                  indicator: BoxDecoration(
+                      color: Colors.cyan,
+                      border: Border(
+                          bottom: BorderSide(
+                        color: Colors.black,
+                        width: 2,
+                      ))),
+                  indicatorColor: Colors.amberAccent,
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.home),
+                      text: 'Home',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.book_online),
+                      text: 'Book',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.shop),
+                      text: 'Shop',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.person),
+                      text: 'Profile',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            title: Text(
+              'Top tab bar sample'.toUpperCase(),
+              style: const TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(
+                Icons.home,
+                color: Colors.black12,
+                size: 100,
+              ),
+              Icon(
+                Icons.book_online,
+                color: Colors.black12,
+                size: 100,
+              ),
+              Icon(
+                Icons.shop,
+                color: Colors.black12,
+                size: 100,
+              ),
+              Icon(
+                Icons.person,
+                color: Colors.black12,
+                size: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class CustomHeightAppBar extends StatelessWidget {
   const CustomHeightAppBar({super.key});
   double getHeight(BuildContext context) => MediaQuery.of(context).size.height;
