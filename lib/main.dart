3bbcd9b0dce43c4d3f4b96d8b3e0cdf837c95f81
Andika Andriana <andriana.mia1@gmail.com>
2023-03-18 18:25:24 +0700

    + Colorful Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter_application_1/colorful_buttton.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,43 +13,173 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: GenerateQrCodeExample(),
+      home: ColorfulButtonView(),
     );
   }
 }
 
-class GenerateQrCodeExample extends StatelessWidget {
-  const GenerateQrCodeExample({super.key});
+class ColorfulButtonView extends StatelessWidget {
+  const ColorfulButtonView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Generate QR Code Example"),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Text(
-              "Scan Me".toUpperCase(),
-              style: const TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Custom Button Transform"),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              ColorfulButton(
+                mainColor: Colors.red,
+                secondColor: Colors.grey,
+                iconData: Icons.home,
               ),
+              ColorfulButton(
+                mainColor: Colors.green,
+                secondColor: Colors.grey,
+                iconData: Icons.book,
+              ),
+              ColorfulButton(
+                mainColor: Colors.purple,
+                secondColor: Colors.grey,
+                iconData: Icons.shop,
+              ),
+              ColorfulButton(
+                mainColor: Colors.blue,
+                secondColor: Colors.grey,
+                iconData: Icons.person,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorfulButton extends StatefulWidget {
+  final Color mainColor, secondColor;
+  final IconData iconData;
+  const ColorfulButton({
+    super.key,
+    required this.mainColor,
+    required this.secondColor,
+    required this.iconData,
+  });
+
+  @override
+  State<ColorfulButton> createState() => _ColorfulButtonState();
+}
+
+class _ColorfulButtonState extends State<ColorfulButton> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: (isPressed) ? pi / 4 : 0.0,
+      child: GestureDetector(
+        onTapDown: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapUp: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapCancel: () {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        child: Material(
+          borderRadius: BorderRadius.circular(15),
+          elevation: (isPressed) ? 2 : 10,
+          shadowColor: (isPressed) ? widget.secondColor : widget.mainColor,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Stack(
+              children: [
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: SizedBox(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(15),
+                      color:
+                          (isPressed) ? widget.secondColor : widget.mainColor,
+                      child: Transform.rotate(
+                        angle: (isPressed) ? -pi / 4 : 0.0,
+                        child: Icon(
+                          widget.iconData,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: SizedBox(
+                      child: Material(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: SizedBox(
+                      child: Material(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(-30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: SizedBox(
+                      child: Material(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(-30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: SizedBox(
+                      child: Material(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          QrImage(
-            version: 6,
-            size: 300,
-            data: 'https://andika-andriana.github.io',
-          ),
-        ],
-      )),
-    ));
+        ),
+      ),
+    );
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,6 +1,43 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class GenerateQrCodeExample extends StatelessWidget {
+  const GenerateQrCodeExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Generate QR Code Example"),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Scan Me".toUpperCase(),
+              style: const TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          QrImage(
+            version: 6,
+            size: 300,
+            data: 'https://andika-andriana.github.io',
+          ),
+        ],
+      )),
+    ));
+  }
+}
 
 class TabBarExample extends StatelessWidget {
   const TabBarExample({super.key});
